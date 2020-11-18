81996e0a4158f87a6d11b0f48549ba6ac18761e0
mholcomb <madison.holcomb@wsu.edu>
2020-11-18 13:19:46 -0800

    Commit 13c: WIP

---
+++ client/build/precache-manifest.84b6e40832131c81d6952b2d57f93aa5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fd9572c17fb84d2dd922bb4b4c9676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bdad389377b0848cd5c",
+    "url": "/static/css/2.d653346d.chunk.css"
+  },
+  {
+    "revision": "39a0dbe45f5ffc899e6f",
+    "url": "/static/css/main.698263f6.chunk.css"
+  },
+  {
+    "revision": "2bdad389377b0848cd5c",
+    "url": "/static/js/2.c376e260.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c376e260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a0dbe45f5ffc899e6f",
+    "url": "/static/js/main.cb802caf.chunk.js"
+  },
+  {
+    "revision": "19793d9fa96413d1c44d",
+    "url": "/static/js/runtime-main.0d575dc6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
