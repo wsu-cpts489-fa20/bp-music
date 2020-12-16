e7ba7a48350887e9afdec254734ced54f5a18f9f
Kate Radford <kaitlin.radford@wsu.edu>
2020-12-15 22:54:35 -0800

    (KR) events page shows events again

--- client/src/components/ModeBar.js
+++ client/src/components/ModeBar.js
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import AppMode from '../AppMode.js';
 
@@ -9,25 +10,17 @@ class ModeBar extends React.Component {
           " invisible" : (this.props.menuOpen ? " ignore-click visible" : " visible"))}>
         <a className={(this.props.mode === AppMode.FEED ? " item-selected" : null)}
             onClick={()=>this.props.changeMode(AppMode.FEED)}>
-          <span className="modebaricon fa fa-th-list"></span>
-          <span className="modebar-text">Coming Up</span>
-        </a>
-        <a className={(this.props.mode === AppMode.ROUNDS || 
-               this.props.mode === AppMode.ROUNDS_EDITROUND || 
-               this.props.mode === AppMode.ROUNDS_LOGROUND ? 
-                  " item-selected" : null)}
-           onClick={()=>this.props.changeMode(AppMode.ROUNDS)}>
-          <span className="modebar-icon  fa fa-music"></span>
+          <span className="modebar-icon fa fa-calendar"></span>
           <span className="modebar-text">Events</span>
         </a>
         <a className={(this.props.mode === AppMode.COURSES ? " item-selected" : null)}
           onClick={()=>this.props.changeMode(AppMode.COURSES)}>
-          <span className="modebar-icon  fa fa-map-marker"></span>
+          <span className="modebar-icon  fa fa-music"></span>
           <span className="modebar-text">Venues</span>
         </a> 
         <a className={(this.props.mode === AppMode.LOCATION_SEARCH ? " item-selected" : null)}
           onClick={()=>this.props.changeMode(AppMode.LOCATION_SEARCH)}>
-          <span className="modebar-icon  fa fa-map-o"></span>
+          <span className="modebar-icon  fa fa-map-marker"></span>
           <span className="modebar-text">Location/Event Finder</span>
         </a> 
         </div>
